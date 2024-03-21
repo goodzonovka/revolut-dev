e3d55e7fd1b20a28ab1edb529f748110492b7ccf
Artem <goodzonovka@gmail.com>
2024-03-21 11:54:07 +0200

    added 'dashboard no data' && 'add goals' pages

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
+import './choices.js';
+
 /* проверка на поддержку webp формата */
 import BaseHelpers from './helpers/BaseHelpers.js';
 
@@ -9,3 +11,43 @@ BaseHelpers.addLoadedClass();
 
 BaseHelpers.headerFixed();
 /* end проверка на поддержку webp формата */
+
+const choicesSelects = document.querySelectorAll('.choices-select-js');
+
+choicesSelects.forEach(function (element) {
+    new Choices(element, {
+        searchEnabled: false, // Отключить поиск, если не нужен
+        itemSelectText: '', // Текст для выбора элемента
+    });
+})
+
+const dateInputs = document.querySelectorAll('.date-input-js');
+
+dateInputs.forEach(function (element) {
+    flatpickr(element, {
+        disableMobile: "true"
+        // mode: "range",
+        // dateFormat: "Y-m-d"
+    });
+});
+
+if (document.getElementById('editor')) {
+    var editor = ace.edit("editor");
+    editor.session.setMode("ace/mode/sql");
+    editor.setTheme("ace/theme/tomorrow_night_blue");
+    if (window.innerWidth < 359) {
+        console.log('q')
+        editor.setFontSize(12);
+    } else if (window.innerWidth < 768) {
+        editor.setFontSize(14);
+    } else {
+        editor.setFontSize(16);
+    }
+}
+
+document.querySelector('.sidebar__btn-open').addEventListener('click', function() {
+    document.querySelector('.sidebar').classList.add('active');
+});
+document.querySelector('.sidebar__close').addEventListener('click', function() {
+    document.querySelector('.sidebar').classList.remove('active');
+});
